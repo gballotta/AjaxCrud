7a13a406eabd93bcfd621fabde81d594a44d68f8
Giovanni Ballotta <giovanni@giovanniballotta.com>
2014-12-30 11:48:13 +0100

    Boilerplate pagina index

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,4 +1,24 @@
 @extends('layouts.main')
 @section('content')
-<h1>Hiya!</h1>
+<div class="row">
+	<div class="col-md-12">
+		<div class="page-header">
+			<h1>AJAX CRUD <small>A Jointhub study</small></h1>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-10">
+		<form>
+			<div class="form-group">
+				<input type="text" class="form-control" id="ricerca" placeholder="Inserire cognome" />
+			</div>
+		</form>
+	</div>
+	<div class="col-md-2">
+		<a href="#" class="btn btn-primary">
+			Crea nuovo
+		</a>
+	</div>
+</div>
 @endsection()
